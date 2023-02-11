b0b7ebf341969e834cef7fa6b5608ba94e87ae53
Netter <2501259757@qq.com>
2023-02-11 15:35:11 +0800

    2023/2/11

---
+++ src/左程云/class08/ReverseStackUsingRecursive.java
@@ -0,0 +1,32 @@
+package 左程云.class08;
+
+import java.util.Stack;
+
+/**
+ * @description:
+ * @author: Netter
+ * @date: 2023-02-11 13:05
+ */
+public class ReverseStackUsingRecursive {
+
+    public static void reverse(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int i = f(stack);
+        reverse(stack);
+        stack.push(i);
+    }
+
+    public static int f(Stack<Integer> stack) {
+        Integer result = stack.pop();
+        if (stack.isEmpty()) {
+            return result;
+        }else {
+            int last = f(stack);
+            stack.push(result);
+            return last;
+        }
+    }
+
+}
